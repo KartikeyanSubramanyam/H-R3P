346406d5641fbd44ca87b3e03345f7cfe848716c
Kartikeyan Subramanyam <kartik@Kartikeyans-Laptop.local>
2024-09-05 12:34:54 -0700

    added map illustration capabilities

--- Cellular Analysis/data_analysis_playground.py
+++ Cellular Analysis/data_analysis_playground.py
@@ -2,6 +2,9 @@ import pandas as pd
 import csv
 from geopy.distance import geodesic
 import numpy as np
+import folium
+
+
 
 # Reading AHA dataset for hospitals and their respective co-ordinates
 # Depending on version of python; might need to run the below command as:
@@ -15,8 +18,20 @@ df = pd.DataFrame(data[6:], columns=data[5])
 hospital_lat_long_df = df[['Hospital Name', 'Latitude', 'Longitude']]
 print(hospital_lat_long_df)
 
+hospital_coords = hospital_lat_long_df[['Latitude', 'Longitude']].values
+hospital_coords_df = pd.DataFrame(hospital_coords, columns=['Latitude', 'Longitude'])
+hospital_names = hospital_lat_long_df['Hospital Name'].values
+print(hospital_names)
+print(hospital_names[1])
+
+# Convert the columns to numeric (floats)
+# hospital_coords_df['Latitude'] = pd.to_numeric(hospital_coords_df['Latitude'])
+# hospital_coords_df['Longitude'] = pd.to_numeric(hospital_coords_df['Longitude'], errors='coerce')
 
 
+hospital_coords_df = hospital_coords_df.apply(pd.to_numeric)
+hospital_coords_df_cleaned = hospital_coords_df.dropna(subset=['Latitude', 'Longitude'])
+print(len(hospital_coords_df_cleaned))
 # Split the DataFrame into 4 parts
 df_split = np.array_split(hospital_lat_long_df, 4)
 
@@ -27,10 +42,10 @@ df3 = df_split[2]
 df4 = df_split[3]
 
 # Print the individual DataFrames
-print("DataFrame 1:\n", df1)
-print("DataFrame 2:\n", df2)
-print("DataFrame 3:\n", df3)
-print("DataFrame 4:\n", df4)
+# print("DataFrame 1:\n", df1)
+# print("DataFrame 2:\n", df2)
+# print("DataFrame 3:\n", df3)
+# print("DataFrame 4:\n", df4)
 
 
 # Extracting OpenCellID Data (7 different files for 310-316 MCC)
@@ -44,7 +59,7 @@ celldata315 = pd.read_csv('315.csv.gz', compression='gzip', header=None, names=c
 celldata316 = pd.read_csv('316.csv.gz', compression='gzip', header=None, names=column_names)
 
 
-print(celldata310)
+# print(celldata310)
 # print(len(celldata311))
 # print(len(celldata312))
 # print(len(celldata313))
@@ -57,13 +72,30 @@ print(len(celldata310)+len(celldata311)+len(celldata312)+len(celldata313)+len(ce
 combined_celldata = [celldata310, celldata311, celldata312, celldata313, celldata314, celldata315, celldata316]
 celldata = pd.concat(combined_celldata, ignore_index=True)
 
+# print(celldata)
 # lengths for separate and concatenated match
-# print(len(celldata))
+print(len(celldata))
 
 filtered_by_range_df = celldata[celldata.iloc[:, 8] <= 15000]
 print(len(filtered_by_range_df))
 fin_celldata = filtered_by_range_df[filtered_by_range_df.iloc[:, 9] > 2]
 print(len(fin_celldata))
 # print(fin_celldata)
+
+# Create a map centered at a specific latitude and longitude
+my_map = folium.Map(location=[39.8097, 98.5556], zoom_start=1)
+
+looprun = 0
+for idx, hospital_coord in enumerate(hospital_coords_df_cleaned.iterrows()):
+    print(hospital_coord[1]['Latitude'])
+    print(hospital_coord[1]['Longitude'])
+    if idx == 0:  # Skip the first row
+        continue
+    # Add a marker for each valid coordinate
+    folium.Marker([hospital_coord[1]['Latitude'], hospital_coord[1]['Longitude']], popup=hospital_names[looprun]).add_to(my_map)
+    looprun += 1
+
+# # Display the map
+my_map.save("HospitalsMap.html")
 
 
