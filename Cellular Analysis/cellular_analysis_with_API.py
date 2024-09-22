ccbb896cfa7572a3339951969e3f03e7b98090b0
Kartikeyan Subramanyam <kartik@Kartikeyans-Laptop.local>
2024-09-22 14:12:55 -0700

    adding new cellular files for more hdos and upadting api results list with around 400 results

--- Cellular Analysis/cellular_analysis_with_API.py
+++ Cellular Analysis/cellular_analysis_with_API.py
@@ -48,9 +48,6 @@ def query_opencellid_and_save_csv(api_key, hospital_name, lat, lon, distance_met
 
 
 
-
-api_key = "pk.dbbe0c1c9071b2b428c86f62d8246dfa"
-
 # Reading AHA dataset for hospitals and their respective co-ordinates
 # Depending on version of python; might need to run the below command as:
 # with open('2024_05_UC_San_Diego_Health.csv', 'r', encoding='utf-8-sig') as f:
@@ -62,36 +59,59 @@ with open('2024_05_UC_San_Diego_Health.csv', 'r') as f:
 df = pd.DataFrame(data[6:], columns=data[5])
 hospital_lat_long_df = df[['Hospital Name', 'Latitude', 'Longitude']]
 print(len(hospital_lat_long_df))
+
 # Clean and convert latitude/longitude to numeric
 hospital_lat_long_df[['Latitude', 'Longitude']] = hospital_lat_long_df[['Latitude', 'Longitude']].apply(pd.to_numeric)
 hospital_lat_long_df_cleaned = hospital_lat_long_df.dropna(subset=['Latitude', 'Longitude'])
 print(len(hospital_lat_long_df_cleaned))
 
-counter = 0
+
+api_key = "pk.dbbe0c1c9071b2b428c86f62d8246dfa"
+counter = 1
 for _, row in hospital_lat_long_df_cleaned.iterrows():
     hospital_name = row['Hospital Name']
     latitude = row['Latitude']
     longitude = row['Longitude']
-    
     print(f"Querying OpenCelliD for {hospital_name} at latitude {latitude}, longitude {longitude}...")
-    
+
     # Query OpenCelliD and save the result as CSV
     query_opencellid_and_save_csv(api_key, hospital_name, latitude, longitude)
-    
-    if counter == 100:
+    if counter == 75:
         api_key = "pk.929ba4918140777ff0fee3a8bd532283"
-    if counter == 200:
+    if counter == 150:
         api_key = "pk.a43267bcc74980475bbe50592e4ef781"
-    if counter == 300:
+    if counter == 225:
         api_key = "pk.4644fdc16dd25203dc5b225d3b97fea1"
-    if counter == 400:
+    if counter == 300:
         api_key = "pk.4e7a2c9626e45c4fdcfddde0b55fa338"
-    if counter == 500: 
+    if counter == 375: 
         api_key = "pk.57a82caf08b0555e8f58073e6b94e508"
-    if counter == 600:
+    if counter == 450:
         api_key = "pk.48054a2b196287b93fba033641e2e869"
-    counter += 1
-    if counter == 700:
+    if counter == 525:
+        api_key = "pk.e130421224b6659d8d452b87084d7eac"
+    if counter == 600:
+        api_key = "pk.2c9ac7b6baf9752e8a8b82db88db80b3"
+    if counter == 675:
         break
 
+    counter += 1
+
+# csv_filename = "WellSpan_Surgery_and_Rehabilitation_Hospital.csv"
+# lat = 39.9263
+# lon = -76.719
+# lat_min, lat_max, lon_min, lon_max = calculate_BBOX(lat, lon)
+#     # OpenCelliD API endpoint and parameters (including format=csv)
+# url = f"http://www.opencellid.org/cell/getInArea?key={api_key}&BBOX={lat_min},{lon_min},{lat_max},{lon_max}&format=csv"
+
+# # Send GET request
+# response = requests.get(url)
+
+# if response.status_code == 200:
+#     # Write the response content (CSV) to a file
+#     with open(csv_filename, 'wb') as file:
+#         file.write(response.content)
+#     print(f"CSV file saved: {csv_filename}")
+# else:
+#     print(f"Error: {response.status_code} for WellSpan_Surgery_and_Rehabilitation_Hospital")
 print("Process completed!")
